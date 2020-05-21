ee1c37fbc94694d79a56eaf039240ce0ab5a5bc5
KHAJI FAIZAN ALI <39943248+khajifaizanali@users.noreply.github.com>
2020-05-21 13:28:17 +0530

    Update api.php

--- api.php
+++ api.php
@@ -4,23 +4,19 @@ $user_id = $_POST['user_id'];
 $module_id = $_POST['module_id'];
 $channel = $_POST['channel'];
 $incoming=$_POST['incoming_message'];
-$incoming='India';
 $file = "https://api.covid19api.com/summary";
 $data = file_get_contents($file);
 $result = json_decode($data, true);
 $obj=$result["Countries"];
 foreach($obj as $key => $value) {
-    $message=$incoming;
-    echo strcmp(strtolower($incoming),$value["Slug"]);
-    echo "\n";
     if(strcmp(strtolower($incoming),$value["Slug"])==0){
-    $message=$message."New cases ".(string)$value["NewConfirmed"]."\n Total confirmed cases".(string)$value["TotalConfirmed"];
+    $message=$incoming." New cases ".(string)$value["NewConfirmed"]."\n Total confirmed cases".(string)$value["TotalConfirmed"];
         break;
     }    
 }
 $new=(string)$result["Global"]["NewConfirmed"];
 $total=(string)$result["Global"]["TotalConfirmed"];
-$message = $message."\nGlobal Report:\nNew cases confirmed are ".$new."\ntotal number of cases are ".$total.$channel.$module_id.$incoming;
+$message = $message."\nGlobal Report:\nNew cases confirmed are ".$new."\ntotal number of cases are ".$total;
 $suggestedReplies = ["India","canada","Indonesia"];
 header ('Content-Type: application/json');
 $response = [
