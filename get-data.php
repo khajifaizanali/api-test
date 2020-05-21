4d76e7a01802298ddbfa846ee01d883c12f5df5e
KHAJI FAIZAN ALI <39943248+khajifaizanali@users.noreply.github.com>
2020-05-21 11:25:00 +0530

    Update get-data.php

--- get-data.php
+++ get-data.php
@@ -4,5 +4,5 @@ $data = file_get_contents($file);
 $data = mb_substr($data, strpos($data, '{'));
 $data = mb_substr($data, 0, -1);
 $result = json_decode($data, true);
-print_r($result['Global']);
+print_r($result["Global"]);
 ?>
