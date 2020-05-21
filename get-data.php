20986abf9a3a5f875da651a75f0298226b96d5a7
KHAJI FAIZAN ALI <39943248+khajifaizanali@users.noreply.github.com>
2020-05-21 11:25:43 +0530

    Update get-data.php

--- get-data.php
+++ get-data.php
@@ -1,8 +1,6 @@
 <?php
 $file = "https://api.covid19api.com/summary";
 $data = file_get_contents($file);
-$data = mb_substr($data, strpos($data, '{'));
-$data = mb_substr($data, 0, -1);
 $result = json_decode($data, true);
 print_r($result["Global"]);
 ?>
